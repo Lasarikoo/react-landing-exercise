814b5ad189755f2ef5269fc330c6c7d80acc2f19
Lasarikoo <lazaro@bega.ltd>
2025-02-19 22:05:14 +0000

    navbar component finished

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,10 +1,43 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function Navbar() {
     return (
-        <nav className="navbar sticky-top bg-dark">
-            <div className="container-fluid text-white">
+        <nav className="navbar navbar-expand-lg sticky-top bg-dark text-white">
+            <div className="container-fluid">
+                {/* Title */}
                 <h3 className="fs-3">Start Bootstrap</h3>
+                {/* Burger button dropdown menu */}
+                <div className="dropdown d-md-none">
+                    <a href="" className="text-white fs-2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <FontAwesomeIcon icon={faBars} />
+                    </a>
+                    {/* Dropdown menu */}
+                    <ul className="bg-dark dropdown-menu dropdown-menu-start menu-fullwidth mt-0">
+                        <li><a className="dropdown-item link-light" href="#">Home</a></li>
+                        <li><a className="dropdown-item link-light" href="#">About</a></li>
+                        <li><a className="dropdown-item link-light" href="#">Services</a></li>
+                        <li><a className="dropdown-item link-light" href="#">Contact</a></li>
+                    </ul>
+                </div>
+                {/* Nav menu */}
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto fs-4">
+                        <li className="nav-item">
+                        <a className="nav-link link-light" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link link-light" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link link-light" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link link-light" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//Font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
+
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
