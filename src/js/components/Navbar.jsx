12fbe56a67cecf6c372a1d7f6379fe03ae106cff
Lasarikoo <lazaro@bega.ltd>
2025-02-19 19:22:24 +0000

    Navbar añadido

---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="navbar sticky-top bg-dark">
+            <div className="container-fluid text-white">
+                <h3 className="fs-3">Start Bootstrap</h3>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
